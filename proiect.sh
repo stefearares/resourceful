e7de67380b119ca178d211fff37be01fd7cd0ff1
DeianGG <49059344+DeianGG@users.noreply.github.com>
2024-01-14 16:24:12 +0200

    rezolvare bug cu 0 si adaugare mem graph proiect.sh

--- proiect.sh
+++ proiect.sh
@@ -31,9 +31,21 @@ tester=$(pwd | rev | cut -d'/' -f1 | rev)
 moment=$(date +"Date: %D Time: %T%n")
 who=$(whoami)
 up=$(uptime -p)
-cpu=$(echo "100 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr ',' '.')" | bc | tr ' ' '0')
+cpua=$(echo "100 - $(mpstat 1 1 | tail -n 1 | rev | cut -d' ' -f1 | rev | tr ',' '.')" | bc)
+cpub=$(echo $cpua | cut -d'.' -f1)
+if [ ${#cpub} -lt 1 ]
+then
+	cpu=0
+	cpu+="."
+	cpu+=$(echo $cpua | cut -d'.' -f2)
+else
+	cpu=$(echo $cpua)
+fi
 totalmem=$(free | awk '/^Mem/ {printf $2}')
 usedmem=$(free | awk '/^Mem/ {printf $3}')
+totalmem1=$((totalmem))
+usedmem1=$((usedmem))
+mem_perc1=$((usedmem * 100 / totalmem))
 pack=$(apt-mark showmanual | wc -l)
 network=$(speedtest-cli --simple 2>/dev/null)
 
@@ -52,7 +64,7 @@ if [ "$tester" == "resourceful" ]; then
   printf "User: %s\n" "$who" >>history
   printf "Current Uptime: %s\n" "$up" >>history
   printf "CPU usage: %s%%\n" "$cpu" >>history
-  printf "Used memory: %s KBs / Total: %s KBs\n" "$usedmem" "$totalmem" >>history
+  printf "Used memory: %s KBs / Total: %s KBs - %s\n" "$usedmem" "$totalmem" "$mem_perc1" >>history
   printf "Manually installed packages: %s\n" "$pack" >>history
   printf "%s\n" "$network" >>history
   echo -n "I/O: " >>history
@@ -134,7 +146,7 @@ echo -e " ____________________"
 
 no=$(cat history | grep CPU | wc -l)
 
-if ((no > 2)); then
+if (($no > 2)); then
   cpus=$(cat history | grep CPU | tail -3 | cut -d' ' -f 3 | cut -d'.' -f1)
 
   cpu1=$(echo $cpus | cut -d' ' -f1)
@@ -146,25 +158,111 @@ if ((no > 2)); then
   cpu3=$(echo $cpus | cut -d' ' -f3)
   [ ${#cpu3} -lt 1 ] && cpu3=0
 
-  if ((cpu1 < 33)); then
+  if (($cpu1 < 33)); then
     forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
-  elif ((cpu1 < 66)); then
+  elif (($cpu1 < 66)); then
     forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
   else
     forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
   fi
 
-  if ((cpu2 < 33)); then
+  if (($cpu2 < 33)); then
     forma2="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |")"
-  elif ((cpu2 < 66)); then
+  elif (($cpu2 < 66)); then
     forma2="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |")"
   else
     forma2="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |")"
   fi
 
-  if ((cpu3 < 33)); then
+  if (($cpu3 < 33)); then
     forma3="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |__")"
-  elif ((cpu3 < 66)); then
+  elif (($cpu3 < 66)); then
+    forma3="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |__")"
+  else
+    forma3="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |__")"
+  fi
+
+  paste -d '' <(echo "$forma1") <(echo "$forma2") <(echo "$forma3")
+elif (($no == 2)); then
+  cpus=$(cat history | grep CPU | tail -2 | cut -d' ' -f 3 | cut -d'.' -f1)
+
+  cpu1=$(echo $cpus | cut -d' ' -f1)
+  [ ${#cpu1} -lt 1 ] && cpu1=0
+
+  cpu2=$(echo $cpus | cut -d' ' -f2)
+  [ ${#cpu2} -lt 1 ] && cpu2=0
+
+  if (($cpu1 < 33)); then
+    forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
+  elif (($cpu1 < 66)); then
+    forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  else
+    forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  fi
+
+  if (($cpu2 < 33)); then
+    forma2="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |__")"
+  elif (($cpu2 < 66)); then
+    forma2="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |__")"
+  else
+    forma2="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |__")"
+  fi
+
+  paste -d '' <(echo "$forma1") <(echo "$forma2")
+else
+  cpu1=$(cat history | grep CPU | tail -1 | cut -d' ' -f 3 | cut -d'.' -f1)
+  [ ${#cpu1} -lt 1 ] && cpu1=0
+
+  if (($cpu1 < 33)); then
+    forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
+  elif (($cpu1 < 66)); then
+    forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  else
+    forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  fi
+
+  echo "$forma1"
+fi
+
+echo -e "\n"
+
+echo -e "  ${purple} Memory usage graph:${nocolor}"
+
+echo -e " ____________________"
+
+no2=$(cat history | grep memory | wc -l)
+
+if (($no2 > 2)); then
+  cpus=$(cat history | grep memory: | rev | cut -d' ' -f1 | rev| tail -3)
+
+  cpu1=$(echo $cpus | cut -d' ' -f1)
+  [ ${#cpu1} -lt 1 ] && cpu1=0
+
+  cpu2=$(echo $cpus | cut -d' ' -f2)
+  [ ${#cpu2} -lt 1 ] && cpu2=0
+
+  cpu3=$(echo $cpus | cut -d' ' -f3)
+  [ ${#cpu3} -lt 1 ] && cpu3=0
+
+  if (($cpu1 < 33)); then
+    forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
+  elif (($cpu1 < 66)); then
+    forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  else
+    forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
+  fi
+
+  if (($cpu2 < 33)); then
+    forma2="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |")"
+  elif (($cpu2 < 66)); then
+    forma2="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |")"
+  else
+    forma2="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |")"
+  fi
+
+  if (($cpu3 < 33)); then
+    forma3="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |__")"
+  elif (($cpu3 < 66)); then
     forma3="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |__")"
   else
     forma3="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |__")"
@@ -180,17 +278,17 @@ elif ((no == 2)); then
   cpu2=$(echo $cpus | cut -d' ' -f2)
   [ ${#cpu2} -lt 1 ] && cpu2=0
 
-  if ((cpu1 < 33)); then
+  if (($cpu1 < 33)); then
     forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
-  elif ((cpu1 < 66)); then
+  elif (($cpu1 < 66)); then
     forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
   else
     forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
   fi
 
-  if ((cpu2 < 33)); then
+  if (($cpu2 < 33)); then
     forma2="$(echo -e "      \n      \n      \n      \n   __ \n  |  |\n__|  |__")"
-  elif ((cpu2 < 66)); then
+  elif (($cpu2 < 66)); then
     forma2="$(echo -e "      \n      \n   __ \n  |  |\n  |  |\n  |  |\n__|  |__")"
   else
     forma2="$(echo -e "   __ \n  |  |\n  |  |\n  |  |\n  |  |\n  |  |\n__|  |__")"
@@ -201,9 +299,9 @@ else
   cpu1=$(cat history | grep CPU | tail -1 | cut -d' ' -f 3 | cut -d'.' -f1)
   [ ${#cpu1} -lt 1 ] && cpu1=0
 
-  if ((cpu1 < 33)); then
+  if (($cpu1 < 33)); then
     forma1="$(echo -e "|      \n|      \n|      \n|      \n|   __ \n|  |  |\n|__|  |")"
-  elif ((cpu1 < 66)); then
+  elif (($cpu1 < 66)); then
     forma1="$(echo -e "|      \n|      \n|   __ \n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
   else
     forma1="$(echo -e "|   __ \n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|  |  |\n|__|  |")"
